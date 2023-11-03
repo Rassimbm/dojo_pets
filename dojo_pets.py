a002ecdbf527c07bc4fd96069a168905b3261139
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2023-11-02 22:55:12 -0500

    Implemented sleep(), eat(), play() and noise() methods on the Pet class.

--- dojo_pets.py
+++ dojo_pets.py
@@ -26,3 +26,19 @@ class Pet:
         self.health = 100
         self.energy = 100
         
+    # sleep() - increases the pets energy by 25
+    def sleep(self):
+        self.energy += 25
+        return self
+    # eat() - increases the pet's energy by 5 & health by 10
+    def eat(self):
+        self.energy += 5
+        self.health += 10
+        return self
+    # play() - increases the pet's health by 5
+    def play(self):
+        self.health += 5
+        return self
+    # noise() - prints out the pet's sound
+    def noise(self):
+        return f"{self.name} is being noisy!!!"
