3db203c05b66b87ab7be39abad7912d855d12fe9
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2023-11-02 22:20:14 -0500

    Added a Ninja class and its instance attributes

---
+++ dojo_pets.py
@@ -0,0 +1,7 @@
+class Ninja:
+    def __init__(self, first_name , last_name , treats , pet_food , pet):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.treats = treats
+        self.pet_food = pet_food
+        self.pet = pet
