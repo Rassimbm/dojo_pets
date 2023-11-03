8a767cc654025d35627efab7845ac2954b6a6e4d
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2023-11-02 23:57:50 -0500

    Make an instance of a Ninja and assign them an instance of a pet to the pet attribute.

--- dojo_pets.py
+++ dojo_pets.py
@@ -1,3 +1,29 @@
+class Pet:
+    def __init__(self , name , type , tricks , noise):
+        self.name = name
+        self.type = type
+        self.tricks = tricks
+        self.health = 100
+        self.energy = 100
+        self.noise = noise
+
+    def sleep(self):
+        self.energy += 25
+        return self
+
+    def eat(self):
+        self.energy += 5
+        self.health += 10
+        return self
+
+    def play(self):
+        self.health += 5
+        return self
+
+    def make_noise(self):
+        print(self.noise)
+        return self
+    
 class Ninja:
     def __init__(self, first_name , last_name , treats , pet_food , pet):
         self.first_name = first_name
@@ -7,38 +33,20 @@ class Ninja:
         self.pet = pet
 
     def walk(self):
-        Pet.play()
+        self.pet.play()
         return self
 
     def feed(self):
-        Pet.eat()
+        self.pet.eat()
+        print(f"{self.first_name} is feeding {self.pet.name}")
         return self
 
     def bathe(self):
-        Pet.noise()
+        self.pet.make_noise()
         return self
     
-class Pet:
-    def __init__(self , name , type , tricks):
-        self.name = name
-        self.type = type
-        self.tricks = tricks
-        self.health = 100
-        self.energy = 100
-        
-    # sleep() - increases the pets energy by 25
-    def sleep(self):
-        self.energy += 25
-        return self
-    # eat() - increases the pet's energy by 5 & health by 10
-    def eat(self):
-        self.energy += 5
-        self.health += 10
-        return self
-    # play() - increases the pet's health by 5
-    def play(self):
-        self.health += 5
-        return self
-    # noise() - prints out the pet's sound
-    def noise(self):
-        return f"{self.name} is being noisy!!!"
+mr_squeaky = Pet("Mr Squeaky" , "Cat" , "Speak" , "Murmuring")
+
+ninja_1 = Ninja("Rassim" , "Benmhamed" , "jerky beef" , "Salmon" , mr_squeaky )
+
+ninja_1.walk().feed().bathe()
